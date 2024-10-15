8f641f8ab0930cdeb2b1ff3a399405bd684dcf09
NikitaSergeevich <nvpetrochenko@edu.hse.ru>
2024-10-15 23:05:32 +0300

    login/register forms are ready

---
+++ actions/login.ts
@@ -0,0 +1,15 @@
+"use server"
+
+import { z } from 'zod'
+import { LoginSchema } from '@/schemas'
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+    const validatedField = LoginSchema.safeParse(values)
+
+    if (!validatedField.success) {
+        return { error: "Invalid Fields" }
+    }
+
+
+    return { success: "Email sent !" }
+}
---
+++ actions/register.ts
@@ -0,0 +1,15 @@
+"use server"
+
+import { z } from 'zod'
+import { RegisterSchema } from '@/schemas'
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+    const validatedField = RegisterSchema.safeParse(values)
+
+    if (!validatedField.success) {
+        return { error: "Invalid Fields" }
+    }
+
+
+    return { success: "Email sent !" }
+}
